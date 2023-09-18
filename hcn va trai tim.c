d76e5bec28c76828975111c0d98a7a5ed3213caf
CongLuu1212 <142959339+CongLuu1212@users.noreply.github.com>
2023-09-18 10:00:46 +0700

    Add files via upload

---
+++ hcn va trai tim.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void hcn(){
+	printf("\n **********");
+	printf("\n *        *");
+	printf("\n *        *");
+	printf("\n **********");
+}
+	void traitim(){
+	printf("\n     ***** *****     ");
+	printf("\n  ***    ***    ***  ");
+    printf("\n ***      *      *** ");
+	printf("\n  ***   *****   ***  ");
+	printf("\n    ***  ***  ***    ");
+	printf("\n       *******       ");
+	printf("\n         ***         ");
+	printf("\n          *          ");
+	}
+int main() {
+	hcn();
+	traitim();
+}
